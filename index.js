ff742ef1e15febdc2da3b8f65e486209908a1c03
behzad012 <jahani.programmer@gmail.com>
2019-03-15 13:31:06 +0330

    add ejs template engine

--- index.js
+++ index.js
@@ -7,9 +7,16 @@ const path = require('path');
 const mongoose = require('mongoose');
 const mock = require('./mock.json');
 const mongooseUnique = require('mongoose-unique-validator');
+
+
+// routes
 const routerRegister = require('./routes/register.js');
-const routerUsers = require('./routes/users');
-const routerGetPhotos = require('./routes/getphotos');
+const routerUsers = require('./routes/users.js');
+const notFound = require('./routes/notFound.js');
+const routerGetPhotos = require('./routes/getphotos.js');
+
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname, 'views'));
 
 app.listen(port,()=>console.log( `server running at port ${port}` ));
 mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true,useCreateIndex: true})
@@ -20,7 +27,11 @@ mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true,useCre
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname,'/public')));
+
+
+
 app.use('/api/', routerRegister);
 app.use('/api/getphotos', routerGetPhotos);
 
 
+app.use('/',notFound);
---
+++ routes/notFound.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+const rootDir = require('./../util/rootDir.js');
+
+
+router.use((req,res,next)=>{
+    res.status(404).render('404', {pageTitle: 'صفحه  پیدا نشد!', flag: true, notfound: true, styleproto: true});
+});
+
+module.exports= router;
