47ec717871e9527d729580e14edcb3aec533a10f
behzad012 <jahani.programmer@gmail.com>
2019-03-15 21:25:48 +0330

    viewa changed and mainnav layout added

--- routes/notFound.js
+++ routes/notFound.js
@@ -6,7 +6,12 @@ const rootDir = require('./../util/rootDir.js');
 
 router.use((req,res,next)=>{
     // res.status(404).render('404', { locals: { pageTitle: 'صفحه  پیدا نشد!', flag: true, notfound: true, styleproto: true} });
-    res.status(404).render('404', { pageTitle: 'صفحه  پیدا نشد!', flag: true, notfound: true, styleproto: true });
+    res.status(404).render('pages/404', {
+        pageTitle: 'صفحه  پیدا نشد!',
+        flag: true,
+        notfound: true,
+        styleproto: true
+    });
 });
 
 module.exports= router;
