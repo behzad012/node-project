9d471bd7c5f8c8fbabb81d51d926e7522331490c
behzad012 <jahani.programmer@gmail.com>
2019-03-15 15:51:42 +0330

    404 page with layout head fixed

--- routes/notFound.js
+++ routes/notFound.js
@@ -5,7 +5,8 @@ const rootDir = require('./../util/rootDir.js');
 
 
 router.use((req,res,next)=>{
-    res.status(404).render('404', {pageTitle: 'صفحه  پیدا نشد!', flag: true, notfound: true, styleproto: true});
+    // res.status(404).render('404', { locals: { pageTitle: 'صفحه  پیدا نشد!', flag: true, notfound: true, styleproto: true} });
+    res.status(404).render('404', { pageTitle: 'صفحه  پیدا نشد!', flag: true, notfound: true, styleproto: true });
 });
 
 module.exports= router;
